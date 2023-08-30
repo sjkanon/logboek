fd62339b4d5a5e4c1c3b93adc7b19519a7afebf3
Sjoerd Kanon <sjoerd@kanon.email>
2023-08-30 21:00:28 +0000

    Aug 30, 2023, 11:00 PM

--- index.php
+++ index.php
@@ -137,8 +137,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-            <p>Don't remember your password? <a href="reset-password.php">Reset now</a>.</p>
+            <p class="login-text">Don't remember your password? <a href="reset-password.php">Reset now</a>.</p>
         </form>
     </div>
 </body>
