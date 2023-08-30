ec1ab0e131ddcbbf23f6985ae00f7ff28106a682
Sjoerd Kanon <sjoerd@kanon.email>
2023-08-30 20:20:45 +0000

    Aug 30, 2023, 10:20 PM

--- index.php
+++ index.php
@@ -100,8 +100,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 <header>
-<img src="../images/AVHTECH-logo.png" alt="Logo AVHTECH" class="AVHTECH">
-<img src="../images/RomTlogo.png" alt="Logo Ronde om Texel 2023" class="romt">
+<div class="row">
+  <div class="column">
+  <img src="../images/AVHTECH-logo.png" alt="Logo AVHTECH" class="AVHTECH">
+  </div>
+  <div class="column">
+  <img src="../images/RomTlogo.png" alt="Logo Ronde om Texel 2023" class="romt">
+  </div>
+</div> 
+
+
 
 </header>
 <div class="container">
