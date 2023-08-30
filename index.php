617d12af621fcb0d50a90b488a267e8dcb392a30
Sjoerd Kanon <sjoerd@kanon.email>
2023-08-30 20:00:29 +0000

    Aug 30, 2023, 10:00 PM

--- index.php
+++ index.php
@@ -100,12 +100,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 <header>
+<img src="../images/AVHTECH-logo.png" alt="Logo AVHTECH">
 
-<h2>Welcome to the logging and registration system of Ronde om texel</h2>
-<h4> Please login in to get our system ready</h4>
 </header>
 <div class="container">
-
+<h2>Welcome to the logging and registration system of Ronde om texel</h2>
+<h4> Please login in to get our system ready</h4>
 
         <?php 
         if(!empty($login_err)){
