4b6f8c5300dcb790483c9157f573675a4d4ac13e
Sjoerd Kanon <sjoerd@kanon.email>
2023-08-30 19:31:02 +0000

    Aug 30, 2023, 9:31 PM

--- index.php
+++ index.php
@@ -100,13 +100,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 <header>
-<h1>Welcome to the logging and registration system of Ronde om texel</h1>
-</br>
-<h2> Please login in to get our system ready</h2>
+
 </header>
-<div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
+<div class="container">
+<h2>Welcome to the logging and registration system of Ronde om texel</h2>
+</br>
+<h4> Please login in to get our system ready</h4>
 
         <?php 
         if(!empty($login_err)){
