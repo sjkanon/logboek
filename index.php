bec1a2b9adaac39bd3f0d3dfffbbd81e160c5948
Sjoerd Kanon (First) <sjoerd.kanon@first.eu>
2023-08-31 07:24:29 +0000

    Aug 31, 2023, 9:24 AM

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>logboeksysteem</title>
+        <title>Loginpage for Ronde om Texel</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
         <link rel="stylesheet" media="screen" href="styles/stylesheet.css" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
