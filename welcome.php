8852259759a777450034dd9d96df99ca3c98fa88
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-01 21:12:30 +0200

    Changed

--- welcome.php
+++ welcome.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>logboeksysteem</title>
+        <title>Dashboard - EventSystem</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
     </head>
@@ -29,17 +29,6 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
       <li class="nav-item">
         <a class="nav-link" href="logout.php">Logout</a>
       </li>
@@ -50,6 +39,8 @@
     </form>
   </div>
 </nav>
-</header>    
+</header>  
+<div class="container">
+</div>  
 </body>
 </html>
