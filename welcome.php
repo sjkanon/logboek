d9bad2902c4347c8e3d46f7afbcafce847cef34c
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-01 21:16:30 +0200

    Test

--- welcome.php
+++ welcome.php
@@ -24,23 +24,22 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">DASHBOARD</a>
+        <a class="nav-link" href="#">dashboard</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">logboek</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">uitgifte</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="logout.php">Logout</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+ 
     </form>
   </div>
 </nav>
-</header>  
-<div class="container">
-</div>  
+</header>   
 </body>
 </html>
