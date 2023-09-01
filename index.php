7a631fcc790efdab1cb4bbf870bf696287d4ab96
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-01 19:20:13 +0000

    Sep 1, 2023, 9:20 PM

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Loginpage for events</title>
+        <title>Loginpage - EventSystem</title>
         <link rel="stylesheet" media="screen" href="styles/stylesheet.css" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     </head>
