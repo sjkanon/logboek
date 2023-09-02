14a4255956ea6bd2bb721bf364a51d234b0323e1
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 14:05:56 +0000

    Sep 2, 2023, 4:05 PM

--- index.php
+++ index.php
@@ -1,156 +1,126 @@
 <?php
-// Initialize the session
 session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
+
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: welcome.php");
     exit;
 }
- 
-// Include config file
+
 require_once "config.php";
- 
-// Define variables and initialize with empty values
+
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
- 
-// Processing form data when form is submitted
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
+
     if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
+        $username_err = "Please enter your username.";
     } else{
         $username = trim($_POST["username"]);
     }
-    
-    // Check if password is empty
+
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter your password.";
     } else{
         $password = trim($_POST["password"]);
     }
-    
-    // Validate credentials
+
     if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
         $sql = "SELECT id, username, password FROM users_new WHERE username = ?";
-        
+
         if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
+
             $param_username = $username;
-            
-            // Attempt to execute the prepared statement
+
             if(mysqli_stmt_execute($stmt)){
-                // Store result
                 mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
+
+                if(mysqli_stmt_num_rows($stmt) == 1){
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
-                            if(password_verify($password, $hashed_password)){
-                                // Password is correct, so start a new session
-                            
-                                // Retrieve the user's group from the database
-                                $sql = "SELECT user_group FROM users_new WHERE username = ?";
-                                $stmt = mysqli_prepare($link, $sql);
-                                mysqli_stmt_bind_param($stmt, "s", $username);
-                                mysqli_stmt_execute($stmt);
-                                mysqli_stmt_bind_result($stmt, $userGroup);
-                                mysqli_stmt_fetch($stmt);
-                                mysqli_stmt_close($stmt);
-                            
-                                // Store user data in session variables
-                                $_SESSION["loggedin"] = true;
-                                $_SESSION["id"] = $id;
-                                $_SESSION["username"] = $username;
-                                $_SESSION["user_group"] = $userGroup;
-                            
-                                // Redirect user to welcome page
-                                header("location: welcome.php");
-                            }
+                        if(password_verify($password, $hashed_password)){
+                            $sql = "SELECT user_group FROM users_new WHERE username = ?";
+                            $stmt = mysqli_prepare($link, $sql);
+                            mysqli_stmt_bind_param($stmt, "s", $username);
+                            mysqli_stmt_execute($stmt);
+                            mysqli_stmt_bind_result($stmt, $userGroup);
+                            mysqli_stmt_fetch($stmt);
+                            mysqli_stmt_close($stmt);
+
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+                            $_SESSION["user_group"] = $userGroup;
+
+                            header("location: welcome.php");
                         } else{
-                            // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password.";
                         }
                     }
                 } else{
-                    // Username doesn't exist, display a generic error message
                     $login_err = "Invalid username or password.";
                 }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
-            // Close statement
             mysqli_stmt_close($stmt);
         }
     }
-    
-    // Close connection
+
     mysqli_close($link);
 }
 ?>
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Loginpage - EventSystem</title>
-        <link rel="stylesheet" media="screen" href="styles/stylesheet.css" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    </head>
-    <body>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Page - EventSystem</title>
+    <link rel="stylesheet" media="screen" href="styles/stylesheet.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 <header>
-
-
-<ul class="brands">
-    <li class="brands__item">
-        <a href="https://avhtech.nl"><img class="avhtech" src="../images/AVHTECH-logo.png" alt="Logo AVHTECH"></img></a>
-    </li>
-    <li class="brands__item">
-        <a href="#"><img src="../images/RomTlogo.png" alt="Logo Evenement"></img>
-</a>
-    </li>
-</ul>
+    <ul class="brands">
+        <li class="brands__item">
+            <a href="https://avhtech.nl"><img class="avhtech" src="../images/AVHTECH-logo.png" alt="Logo AVHTECH"></img></a>
+        </li>
+        <li class="brands__item">
+            <a href="#"><img src="../images/RomTlogo.png" alt="Logo Evenement"></img></a>
+        </li>
+    </ul>
 </header>
 <div class="container">
     <div class="login-text">
-<h2>Welcome to the event system</h2>
-<h4> Please login in to get our system ready</h4></div>
-
-        <?php 
-        if(!empty($login_err)){
-            echo '<div class="alert alert-danger">' . $login_err . '</div>';
-        }        
-        ?>
-
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <p class="password-tools">Don't remember your password? <a href="reset-password.php">Reset now</a>.</p>
-            <p class="powered-by"> powered by Avhtech</p>
-    </br>
-        </form>
+        <h2>Welcome to the Event System</h2>
+        <h4>Please log in to access our system</h4>
     </div>
+    <?php
+    if(!empty($login_err)){
+        echo '<div class="alert alert-danger">' . $login_err . '</div>';
+    }
+    ?>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+            <span class="invalid-feedback"><?php echo $username_err; ?></span>
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+            <span class="invalid-feedback"><?php echo $password_err; ?></span>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Login">
+        </div>
+        <p class="password-tools">Forgot your password? <a href="reset-password.php">Reset it now</a>.</p>
+        <p class="powered-by">Powered by Avhtech</p>
+    </form>
+</div>
 </body>
 </html>
