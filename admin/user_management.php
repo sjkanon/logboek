393f37c5e059223ad6c97029a0710a51acf5cb37
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 19:05:45 +0000

    Sep 2, 2023, 9:05 PM

--- admin/user_management.php
+++ admin/user_management.php
@@ -53,6 +53,13 @@ $result = mysqli_query($link, $sql);
     <link rel="stylesheet" media="screen" href="../styles/stylesheet.css" />
     <link rel="stylesheet" media="screen" href="../styles/styles.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- Bootstrap JS (optional, but required for responsive features) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
