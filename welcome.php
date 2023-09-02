4cada1ab2e0c4db737f250224961d9efbe192c77
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 14:03:30 +0000

    Sep 2, 2023, 4:03 PM

--- welcome.php
+++ welcome.php
@@ -1,90 +1,5 @@
 <?php
-// Initialize the session
-session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
-    exit;
-}
- 
-// Include config file
-require_once "config.php";
- 
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = $login_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users_new WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                       // ... (your authentication logic)
-                      if(password_verify($password, $hashed_password)){
-                      // Password is correct, so start a new session
-                      // Retrieve the user's group from the database
-                      $sql = "SELECT grouptype FROM users_new WHERE username = ?";
-                      $stmt = mysqli_prepare($link, $sql);
-                      mysqli_stmt_bind_param($stmt, "s", $username);
-                      mysqli_stmt_execute($stmt);
-                      mysqli_stmt_bind_result($stmt, $userGroup);
-                      mysqli_stmt_fetch($stmt);
-                      mysqli_stmt_close($stmt);
 
-                      // Store user data in session variables
-                      $_SESSION["loggedin"] = true;
-                      $_SESSION["id"] = $id;
-                      $_SESSION["username"] = $username;
-                      $_SESSION["grouptype"] = $userGroup;
-
-                      // Redirect user to welcome page
-                      header("location: welcome.php");
-                    }
-
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
-    // Close connection
-    mysqli_close($link);
-}
 ?>
 <!doctype html>
 <html lang="en">
@@ -108,7 +23,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-    <nav>
+      <nav>
         <ul>
             <?php include 'navbar_logic.php'; ?>
         </ul>
