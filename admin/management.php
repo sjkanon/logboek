7809feeb6c95029fcc085d21ded34bbec202587e
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 17:55:23 +0000

    Sep 2, 2023, 7:55 PM

--- admin/management.php
+++ admin/management.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once "config.php";
+require_once "../config.php";
 
 // Edit User Logic
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["edit_id"])) {
