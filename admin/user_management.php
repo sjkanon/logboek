7df367258d1c0e96ab5d34b864ff778e2184f0b1
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 19:37:34 +0000

    Sep 2, 2023, 9:37 PM

--- admin/user_management.php
+++ admin/user_management.php
@@ -111,7 +111,15 @@ $result = mysqli_query($link, $sql);
                         <?php } ?>
                         <li class="nav-item">
                             <a class="nav-link" href="../logout.php">Logout</a>
-                            </li>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.php">Register</a>
+                        </li>
+                    <?php } ?>
                 </ul>
             </div>
     </nav>
