2ce255f5978cc22aa1dd1b8c6201332527400d8a
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 18:02:01 +0000

    Sep 2, 2023, 8:02 PM

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,14 +1,27 @@
-<!-- Add User Form -->
-<h2>Add User</h2>
-<form method="post" action="">
-    <label>Username: <input type="text" name="add_username" required></label><br>
-    <label>Password: <input type="password" name="add_password" required></label><br>
-    <label>Group Type:
-        <select name="add_grouptype" required>
-            <option value="Admin">Admin</option>
-            <option value="User">User</option>
-            <!-- Add more options as needed -->
-        </select>
-    </label><br>
-    <input type="submit" value="Add User">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard - EventSystem</title>
+    <link rel="stylesheet" media="screen" href="styles/stylesheet.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Add User</h1>
+
+    <!-- Add User Form -->
+    <form method="post" action="add_user_process.php">
+        <label>Username: <input type="text" name="add_username" required></label><br>
+        <label>Password: <input type="password" name="add_password" required></label><br>
+        <label>Group Type:
+            <select name="add_grouptype" required>
+                <option value="Admin">Admin</option>
+                <option value="User">User</option>
+                <!-- Add more options as needed -->
+            </select>
+        </label><br>
+        <input type="submit" value="Add User">
+    </form>
+</body>
+</html>
