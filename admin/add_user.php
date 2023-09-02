1c18b4b5c0e0f986c695d1c1670892865302b083
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 19:35:50 +0000

    Sep 2, 2023, 9:35 PM

--- admin/add_user.php
+++ admin/add_user.php
@@ -1,38 +1,42 @@
-<!-- Edit User Form -->
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["edit_id"])) {
-    $edit_id = $_GET["edit_id"];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit User | EventSystem</title>
+    <link rel="stylesheet" media="screen" href="../styles/stylesheet.css" />
+    <link rel="stylesheet" media="screen" href="../styles/styles.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Add User</h1>
 
-    // Retrieve user details from the database
-    $edit_sql = "SELECT id, username, grouptype FROM users_new WHERE id=?";
-    $edit_stmt = mysqli_prepare($link, $edit_sql);
-    mysqli_stmt_bind_param($edit_stmt, "i", $edit_id);
-    mysqli_stmt_execute($edit_stmt);
-    mysqli_stmt_bind_result($edit_stmt, $edit_id, $edit_username, $edit_grouptype);
-    mysqli_stmt_fetch($edit_stmt);
-    mysqli_stmt_close($edit_stmt);
-    ?>
+        <form method="post" action="add_user_process.php">
+            <div class="form-group">
+                <label for="add_username">Username</label>
+                <input type="text" id="add_username" name="add_username" required>
+            </div>
+            <div class="form-group">
+                <label for="add_password">Password</label>
+                <input type="password" id="add_password" name="add_password" required>
+            </div>
+            <div class="form-group">
+                <label for="add_grouptype">Group Type</label>
+                <select id="add_grouptype" name="add_grouptype" required>
+                    <option value="Admin">Admin</option>
+                    <option value="Logboek">Logboek User</option>
+                    <option value="uitgifte">Uitleen User</option>
+                    <option value="uluser">Uitleen en Logboek User</option>
+                    <!-- Add more options as needed -->
+                </select>
+            </div>
+            <button type="submit">Add User</button>
+        </form>
 
-    <h2>Edit User</h2>
-    <form method="post" action="">
-        <input type="hidden" name="edit_id" value="<?php echo $edit_id; ?>">
-        <div class="form-group">
-            <label for="new_username">Username</label>
-            <input type="text" id="new_username" name="new_username" value="<?php echo $edit_username; ?>" required>
+        <div class="back-button">
+            <a href="user_management.php">Back to User List</a>
         </div>
-        <div class="form-group">
-            <label for="new_grouptype">Group Type</label>
-            <select id="new_grouptype" name="new_grouptype">
-                <option value="admin" <?php if ($edit_grouptype === "admin") echo "selected"; ?>>Admin</option>
-                <option value="logboek" <?php if ($edit_grouptype === "logboek") echo "selected"; ?>>Logboek</option>
-                <option value="uitgifte" <?php if ($edit_grouptype === "uitgifte") echo "selected"; ?>>Uitgifte</option>
-                <option value="uluser" <?php if ($edit_grouptype === "uluser") echo "selected"; ?>>UL User</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Save Changes">
-        </div>
-    </form>
-<?php
-}
-?>
+    </div>
+</body>
+</html>
