b905c722ec193521950fa9376fc044be91ccba63
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 19:01:18 +0000

    Sep 2, 2023, 9:01 PM

--- admin/add_user_process.php
+++ admin/add_user_process.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once "config.php";
+require_once "../config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["add_username"])) {
     $add_username = $_POST["add_username"];
