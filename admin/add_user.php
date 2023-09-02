ea58d6474c9156f3197610987f9ba4aa82adc883
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 19:09:39 +0000

    Sep 2, 2023, 9:09 PM

--- admin/add_user.php
+++ admin/add_user.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add User | EventSystem</title>
+    <title>Edit User | EventSystem</title>
+    <link rel="stylesheet" media="screen" href="../styles/stylesheet.css" />
+    <link rel="stylesheet" media="screen" href="../styles/styles.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
