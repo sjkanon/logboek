b4604c52affb212365717e1111af7030b34cf6e9
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 18:55:57 +0000

    Sep 2, 2023, 8:55 PM

--- admin/add_user.php
+++ admin/add_user.php
@@ -2,28 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard - EventSystem</title>
-    <link rel="stylesheet" media="screen" href="styles/stylesheet.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add User | EventSystem</title>
 </head>
 <body>
-    <h1>Add User</h1>
+    <div class="container">
+        <h1>Add User</h1>
 
-    <!-- Add User Form -->
-    <form method="post" action="add_user_process.php">
-        <label>Username: <input type="text" name="add_username" required></label><br>
-        <label>Password: <input type="password" name="add_password" required></label><br>
-        <label>Group Type:
-            <select name="add_grouptype" required>
-                <option value="Admin">Admin</option>
-                <option value="Logboek">User</option>
-                <option value="Logboek">User</option>
-                <option value="Logboek">User</option>
-                <!-- Add more options as needed -->
-            </select>
-        </label><br>
-        <input type="submit" value="Add User">
-    </form>
+        <form method="post" action="add_user_process.php">
+            <div class="form-group">
+                <label for="add_username">Username</label>
+                <input type="text" id="add_username" name="add_username" required>
+            </div>
+            <div class="form-group">
+                <label for="add_password">Password</label>
+                <input type="password" id="add_password" name="add_password" required>
+            </div>
+            <div class="form-group">
+                <label for="add_grouptype">Group Type</label>
+                <select id="add_grouptype" name="add_grouptype" required>
+                    <option value="Admin">Admin</option>
+                    <option value="User">User</option>
+                    <!-- Add more options as needed -->
+                </select>
+            </div>
+            <button type="submit">Add User</button>
+        </form>
+
+        <div class="back-button">
+            <a href="user_management.php">Back to User List</a>
+        </div>
+    </div>
 </body>
 </html>
