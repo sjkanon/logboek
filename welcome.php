67aca6eeaccd3a86c0cd34a8452cd4f0a0773d63
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 14:32:32 +0000

    Sep 2, 2023, 4:32 PM

--- welcome.php
+++ welcome.php
@@ -11,14 +11,43 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">EventSystem</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <?php include 'navbar_logic.php'; ?>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="welcome.php">Home</a>
+                        </li>
+                        <?php if ($_SESSION["grouptype"] === "admin") { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="admin-dashboard.php">Admin Dashboard</a>
+                            </li>
+                        <?php } elseif ($_SESSION["grouptype"] === "user") { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="user-profile.php">User Profile</a>
+                            </li>
+                        <?php } ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Logout</a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.php">Register</a>
+                        </li>
+                    <?php } ?>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
     </header>
     <!-- Your body content here -->
 </body>
