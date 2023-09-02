7ba815d803464196e7163865dc34bb754fb9fb89
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 18:03:15 +0000

    Sep 2, 2023, 8:03 PM

--- admin/management.php
+++ admin/management.php
@@ -49,11 +49,68 @@ $result = mysqli_query($link, $sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard - EventSystem</title>
+    <title>Admin management - EventSystem</title>
     <link rel="stylesheet" media="screen" href="styles/stylesheet.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <!-- ... HTML content ... -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">EventSystem</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="welcome.php">Home</a>
+                        </li>
+                        <?php if ($_SESSION["grouptype"] === "admin") { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/admin/management.php">Admin Management</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="uitgifte.php">Uitgifte</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="logboek.php">Logboek</a>
+                            </li>
+                        <?php } elseif ($_SESSION["grouptype"] === "logboek") { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="logboek.php">Logboek</a>
+                            </li>
+                            <?php } elseif ($_SESSION["grouptype"] === "uitgifte") { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="uitgifte.php">Uitgifte</a>
+                            </li>
+                            <?php } elseif ($_SESSION["grouptype"] === "uluser") { ?>
+                              <li class="nav-item">
+                                <a class="nav-link" href="logboek.php">Logboek</a>
+                            </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="uitgifte.php">Uitgifte</a>
+                            </li>
+                        <?php } ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Logout</a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.php">Register</a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    </header>
+    <footer>
+    <p>&copy; 2023 Sjoerd Kanon by AvhTech. All rights reserved.</p>
+</footer>
 </body>
 </html>
