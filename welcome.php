8bef9ed2bd86dbad6504b523b3690f88c7ed6aab
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-02 14:14:20 +0000

    Sep 2, 2023, 4:14 PM

--- welcome.php
+++ welcome.php
@@ -1,38 +1,20 @@
-<?php
-
-?>
 <!doctype html>
 <html lang="en">
 <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Dashboard - Event System</title>
-        <link rel="stylesheet" media="screen" href="styles/stylesheet.css" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    </head>
-    <body>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
-<header>
-  <h1>This page is  needed so you can go to very different sites</h1>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <nav>
-        <ul>
-            <?php include 'navbar_logic.php'; ?>
-        </ul>
-    </nav>
-    </ul>
- 
-    </form>
-  </div>
-</nav>
-</header>   
+    <!-- Your head content here -->
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <?php include 'navbar_logic.php'; ?>
+            </div>
+        </nav>
+    </header>
+    <!-- Your body content here -->
 </body>
 </html>
+
