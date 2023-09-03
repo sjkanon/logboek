f6a41799ce8ee603955f94eeb0b2f71c86e22a06
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-03 13:06:03 +0000

    Sep 3, 2023, 3:06 PM

--- add.php
+++ add.php
@@ -7,16 +7,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $waar = $_POST["waar"];
     $message = $_POST["message"];
 
-    // Create a connection to the database
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
     // Insert data into the table
-    $sql = "INSERT INTO user_data (wie, wat, waar, message) VALUES ('$wie', '$wat', '$waar', '$message')";
+    $sql = "INSERT INTO logboek (wie, wat, waar, message) VALUES ('$wie', '$wat', '$waar', '$message')";
 
     if ($conn->query($sql) === TRUE) {
         header("Location: index.php"); // Redirect back to index page
