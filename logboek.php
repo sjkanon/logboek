9fda7a47f1d521cdf376ccfe8118618052b3aebd
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-03 13:51:01 +0000

    Sep 3, 2023, 3:51 PM

--- logboek.php
+++ logboek.php
@@ -1,10 +1,43 @@
 <?php
 session_start();
+require_once "config.php";
 
+// Function to display user navigation based on session status and group type
+function displayUserNavigation() {
+    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+        // Display user-specific navigation
+        if ($_SESSION["grouptype"] === "admin") {
+            echo '<li class="nav-item">';
+            echo '<a class="nav-link" href="/admin/management.php">Admin Management</a>';
+            echo '<ul class="submenu">';
+            echo '<li class="nav-item"><a class="nav-link" href="/admin/user_management.php">User Management</a></li>';
+            // Add more sub-menu items as needed
+            echo '</ul>';
+            echo '</li>';
+            echo '<li class="nav-item"><a class="nav-link" href="uitgifte.php">Uitgifte</a></li>';
+            echo '<li class="nav-item"><a class="nav-link" href="logboek.php">Logboek</a></li>';
+        } elseif ($_SESSION["grouptype"] === "logboek") {
+            echo '<li class="nav-item"><a class="nav-link" href="logboek.php">Logboek</a></li>';
+        } elseif ($_SESSION["grouptype"] === "uitgifte") {
+            echo '<li class="nav-item"><a class="nav-link" href="uitgifte.php">Uitgifte</a></li>';
+        } elseif ($_SESSION["grouptype"] === "uluser") {
+            echo '<li class="nav-item"><a class="nav-link" href="logboek.php">Logboek</a></li>';
+            echo '<li class="nav-item"><a class="nav-link" href="uitgifte.php">Uitgifte</a></li>';
+        }
+        echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
+    } else {
+        // Display navigation for guests
+        echo '<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>';
+        echo '<li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>';
+    }
+}
 
+// Retrieve data from the "logboek" table
+$sql = "SELECT * FROM logboek";
+$result = $conn->query($sql);
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,117 +46,61 @@ session_start();
     <link rel="stylesheet" media="screen" href="styles/stylesheet.css" />
     <link rel="stylesheet" media="screen" href="styles/styles.css" />
     <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-<!-- Bootstrap JS (optional, but required for responsive features) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Bootstrap JS (optional, but required for responsive features) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
     <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">EventSystem</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
-                    <?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="welcome.php">Home</a>
-                        </li>
-                        <?php if ($_SESSION["grouptype"] === "admin") { ?>
-                          <li class="nav-item">
-                            <a class="nav-link" href="/admin/management.php">Admin Management</a>
-                             <ul class="submenu">
-                               <li class="nav-item">
-                                   <a class="nav-link" href="/admin/user_management.php">User Management</a>
-                               </li>
-                        <!-- Add more sub-menu items as needed -->
-                             </ul>
-                          </li>
-                          <li class="nav-item">
-                              <a class="nav-link" href="uitgifte.php">Uitgifte</a>
-                          </li>
-                          <li class="nav-item">
-                                <a class="nav-link" href="logboek.php">Logboek</a>
-                          </li>
-                        <?php } elseif ($_SESSION["grouptype"] === "logboek") { ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="logboek.php">Logboek</a>
-                            </li>
-                            <?php } elseif ($_SESSION["grouptype"] === "uitgifte") { ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="uitgifte.php">Uitgifte</a>
-                            </li>
-                            <?php } elseif ($_SESSION["grouptype"] === "uluser") { ?>
-                              <li class="nav-item">
-                                <a class="nav-link" href="logboek.php">Logboek</a>
-                            </li>
-                              <li class="nav-item">
-                                <a class="nav-link" href="uitgifte.php">Uitgifte</a>
-                            </li>
-                        <?php } ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Logout</a>
-                        </li>
-                    <?php } else { ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="login.php">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="register.php">Register</a>
-                        </li>
-                    <?php } ?>
+                    <?php displayUserNavigation(); ?>
                 </ul>
             </div>
-    </nav>
+        </nav>
     </header>
     <div class="container">
-    <div class="add-user-button">
+        <div class="add-user-button">
             <a href="add_form.php" class="btn">Add Data</a>
         </div>
-    <!-- View/Search Form -->
-    <h2>View/Search Data</h2>
-    <form action="view.php" method="get">
-        Search: <input type="text" name="search_query">
-        <input type="submit" value="Search">
-    </form>
+        <!-- View/Search Form -->
+        <h2>View/Search Data</h2>
+        <form action="view.php" method="get">
+            Search: <input type="text" name="search_query">
+            <input type="submit" value="Search">
+        </form>
 
-    <!-- Display Data -->
-    <h2>Stored Data</h2>
-    <?php
-    require_once "config.php";
-
-    // Retrieve all data from the table
-    $sql = "SELECT * FROM logboek";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        echo "<ul>";
-        while ($row = $result->fetch_assoc()) {
-            echo "<li>";
-            echo "Wie: " . $row["wie"] . ", ";
-            echo "Wat: " . $row["wat"] . ", ";
-            echo "Waar: " . $row["waar"] . ", ";
-            echo "Message: " . $row["message"] . ", ";
-            echo "Created: " . $row["created"] . ", ";
-            echo "Updated: " . $row["update_time"];
-            echo "</li>";
+        <!-- Display Data -->
+        <h2>Stored Data</h2>
+        <?php
+        if ($result->num_rows > 0) {
+            echo "<ul>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<li>";
+                echo "Wie: " . $row["wie"] . ", ";
+                echo "Wat: " . $row["wat"] . ", ";
+                echo "Waar: " . $row["waar"] . ", ";
+                echo "Message: " . $row["message"] . ", ";
+                echo "Created: " . $row["created"] . ", ";
+                echo "Updated: " . $row["update_time"];
+                echo "</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo "No data available.";
         }
-        echo "</ul>";
-    } else {
-        echo "No data available.";
-    }
-
-    $conn->close();
-    ?>
+        ?>
     </div>
     <footer>
-    <p>&copy; 2023 Sjoerd Kanon by AvhTech. All rights reserved.</p>
-</footer>
+        <p>&copy; 2023 Sjoerd Kanon by AvhTech. All rights reserved.</p>
+    </footer>
 </body>
 </html>
