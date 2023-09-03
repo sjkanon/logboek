ee26fa869dbf701178b8ba7cef91dd0d4da73b87
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-03 14:16:37 +0000

    Sep 3, 2023, 4:16 PM

--- admin/user_management.php
+++ admin/user_management.php
@@ -1,6 +1,11 @@
 <?php
 require_once "../config.php";
 session_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Edit User Logic
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["edit_id"])) {
     $edit_id = $_POST["edit_id"];
