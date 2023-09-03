781f44c570d74752514b00f66b7a13e711159b30
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-03 12:13:24 +0000

    Sep 3, 2023, 2:13 PM

--- logboek.php
+++ logboek.php
@@ -3,3 +3,28 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP CRUD Example with MySQL</title>
+</head>
+<body>
+    <h1>Simple PHP CRUD Example with MySQL</h1>
+
+    <!-- Add Form -->
+    <h2>Add Data</h2>
+    <form action="add.php" method="post">
+        Name: <input type="text" name="name"><br>
+        Email: <input type="email" name="email"><br>
+        <input type="submit" value="Add">
+    </form>
+
+    <!-- View/Search Form -->
+    <h2>View/Search Data</h2>
+    <form action="view.php" method="get">
+        Search: <input type="text" name="search_query">
+        <input type="submit" value="Search">
+    </form>
+</body>
+</html>
