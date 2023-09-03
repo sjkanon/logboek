74264502358b38e8c32716046665828268effc76
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-03 13:28:49 +0000

    Sep 3, 2023, 3:28 PM

--- add_form.php
+++ add_form.php
@@ -10,17 +10,14 @@
 </head>
 <body>
     <div class="container">
-        <h1>Add User</h1>
+        <h1>Add Data to logboek</h1>
 
         <form method="post" action="add.php">
             <div class="form-group">
                 <label for="add_wie">Wie</label>
                 <input type="text" id="add_wie" name="add_wie" required>
             </div>
-            <div class="form-group">
-                <label for="add_wat">Wat</label>
-                <input type="text" id="add_wat" name="add_wat" required>
-            </div>
+            
             <div class="form-group">
                 <label for="add_wat">Wat</label>
                 <select id="add_wat" name="add_wat" required>
@@ -29,8 +26,11 @@
                     <option value="Overig">Overig</option>
                     <!-- Add more options as needed -->
                 </select>
-            </div>
-            <button type="submit">Add User</button>
+                </div>
+                <div class="form-group">
+                <label for="add_waar">Waar</label>
+                <input type="text" id="add_wat" name="add_wat" required>
+                <button type="submit">Add User</button>
         </form>
 
         <div class="back-button">
