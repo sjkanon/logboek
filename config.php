963d0b5c5aeaee2a3fb451507cf0bc98649ac6bd
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-03 14:06:21 +0000

    Sep 3, 2023, 4:06 PM

--- config.php
+++ config.php
@@ -1,16 +1,15 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+// Database credentials
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'logboek');
 define('DB_PASSWORD', 'uYf6d6~94');
 define('DB_NAME', 'kanonict_logboek');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Attempt to connect to MySQL database
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("ERROR: Could not connect. " . $conn->connect_error);
 }
 ?>
