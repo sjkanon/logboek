537ed9d6076f47ae7406075f6d667fcfa9654348
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-03 13:43:05 +0000

    Sep 3, 2023, 3:43 PM

--- add.php
+++ add.php
@@ -1,24 +1,40 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 require_once "config.php";
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $add_wie = $_POST["add_wie"];
-    $add_wat = $_POST["add_wat"];
-    $add_waar = $_POST["add_waar"];
-    $add_message = $_POST["add_message"];
+    $wie = $_POST["add_wie"];
+    $wat = $_POST["add_wat"];
+    $waar = $_POST["add_waar"];
+    $message = $_POST["add_message"];
 
-    // Insert data into the table
-    $insertsql = "INSERT INTO logboek (wie, wat, waar, message) VALUES (?, ?, ?)";
-    $insert_stmt = mysqli_prepare($link, $insert_sql);
-    mysqli_stmt_bind_param($insert_stmt, $add_wie, $add_wat, $add_waar, $add_message);
-    mysqli_stmt_execute($insert_stmt);
-    mysqli_stmt_close($insert_stmt);
-   // Redirect back to user list or do other actions
-   header("Location: logboek.php");
-   exit();
+    // Create a connection to the database
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare the SQL statement
+    $insert_sql = $conn->prepare("INSERT INTO user_data (wie, wat, waar, message) VALUES (?, ?, ?, ?)");
+
+    if ($insert_sql) {
+        // Bind parameters to the prepared statement
+        $insert_sql->bind_param("ssss", $wie, $wat, $waar, $message);
+
+        // Execute the statement
+        if ($insert_sql->execute()) {
+            echo "Data added successfully!";
+        } else {
+            echo "Error: " . $insert_sql->error;
+        }
+
+        // Close the statement
+        $insert_sql->close();
+    } else {
+        echo "Error: " . $conn->error;
+    }
+
+    $conn->close();
 }
-
-mysqli_close($link);
 ?>
