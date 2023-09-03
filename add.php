842600b32a8b95eb9c929c00e65b7c00319054e8
Sjoerd Kanon <sjoerd@kanon.email>
2023-09-03 13:39:30 +0000

    Sep 3, 2023, 3:39 PM

--- add.php
+++ add.php
@@ -3,28 +3,22 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once "config.php";
-
-// Debug: Display connection details
-echo "Server: $servername, Username: $username, Password: $password, Database: $dbname";
-
-// Create a connection to the database
-$conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $wie = $_POST["wie"];
-    $wat = $_POST["wat"];
-    $waar = $_POST["waar"];
-    $message = $_POST["message"];
+    $add_wie = $_POST["add_wie"];
+    $add_wat = $_POST["add_wat"];
+    $add_waar = $_POST["add_waar"];
+    $add_message = $_POST["add_message"];
 
     // Insert data into the table
-    $sql = "INSERT INTO logboek (wie, wat, waar, message) VALUES ('$wie', '$wat', '$waar', '$message')";
-
-    if ($conn->query($sql) === TRUE) {
-        header("Location: logboek.php"); // Redirect back to index page
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
-    $conn->close();
+    $insertsql = "INSERT INTO logboek (wie, wat, waar, message) VALUES (?, ?, ?)";
+    $insert_stmt = mysqli_prepare($link, $insert_sql);
+    mysqli_stmt_bind_param($insert_stmt, "sss", $add_wie, $add_wat, $add_waar, $add_message);
+    mysqli_stmt_execute($insert_stmt);
+    mysqli_stmt_close($insert_stmt);
+   // Redirect back to user list or do other actions
+   header("Location: logboek.php");
+   exit();
 }
+
+mysqli_close($link);
 ?>
